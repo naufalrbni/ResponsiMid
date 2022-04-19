2de144d8e20f6b3d2e68f445775b4ff71623740a
naufalrbni <naufalrbbnii@gmail.com>
2022-04-19 16:32:31 +0700

    inisialisasi project

---
+++ Karyawan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4281
+{
+    internal class Karyawan
+    {
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan(int Nik, string Nama, int GajiBulanan)
+        {
+            this.Nik = Nik;
+            this.Nama = Nama;
+
+            if (GajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else
+            {
+                this.GajiBulanan = GajiBulanan;
+            }
+        }
+
+        public void ShowKaryawan()
+        {
+            Console.WriteLine("{0} \t {1} \t {2}", Nik, Nama, GajiBulanan);
+        }
+
+        public void NaikGaji()
+        {
+            double tmp = 0;
+            tmp = 0.1 * GajiBulanan;
+            GajiBulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{0} \t {1} \t {2}", Nik, Nama, GajiBulanan);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4281
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan Karyawan = new Karyawan(190302123, "Paijo", 3000000);
+            Karyawan Karyawan2 = new Karyawan(190302124, "Jono", 2000000);
+
+            Console.WriteLine("NIK/Nama \t Gaji Bulanan");
+            Console.WriteLine("--------------------------------");
+            Karyawan.ShowKaryawan();
+            Karyawan2.ShowKaryawan();
+
+            Console.WriteLine("\n\nAsyiiiiik kenaikan gaji 10%!!");
+            Console.WriteLine();
+
+            Console.WriteLine("NIK/Nama \t Gaji Bulanan");
+            Console.WriteLine("--------------------------------");
+            Karyawan.NaikGaji();
+            Karyawan2.NaikGaji();
+
+            Console.ReadKey();
+        }
+    }
+}
